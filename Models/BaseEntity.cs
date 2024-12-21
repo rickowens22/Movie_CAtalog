1c4342fd476b28800a4c2c4a884fc8f745c36b06
Peter plodder <billybutcher2281998@gmail.com>
2024-12-21 07:46:28 +0300

    second commit

--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -1,7 +1,7 @@
 using System;
 
 namespace MovieCatalog.Models {
-    //асбтрактный класс для всех сущностей
+    //асбтрактный класс для всех моделей данных
     public abstract class BaseEntity {
         public int Id {get; set; }
     }  
